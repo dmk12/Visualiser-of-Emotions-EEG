6ce5aa2cb4dd23f5e9883f02f08dae330dbead25
Admin <Admin@192.168.2.110>
2014-01-06 10:54:00 +0000

    formatting, var name blk changed to blink, opacity removed

--- EmoApp/src/emoapp/EmoApp.java
+++ EmoApp/src/emoapp/EmoApp.java
@@ -6,7 +6,6 @@ import processing.core.PShape;
 import com.sun.jna.Pointer;
 import com.sun.jna.ptr.IntByReference;
 
-
 public class EmoApp extends PApplet {
 
 	// Emo Headset variables
@@ -19,23 +18,23 @@ public class EmoApp extends PApplet {
 	int option = 2;
 	int state = 0;
 
-	//emotion variables
-	float exc = 0;//excitement
-	float eng = 0;//engagement/boredom
-	float med = 0;//meditation
-	float frs = 0;//frustration
-	//blink
+	// emotion variables
+	float exc = 0;// excitement
+	float eng = 0;// engagement/boredom
+	float med = 0;// meditation
+	float frs = 0;// frustration
+	// blink
 	int blink = 0;
-	float blkX;
-	float blkY;
-	//circles to represent each emotion
+	float blinkX;
+	float blinkY;
+	// circles to represent each emotion
 	private PShape excCrc;
 	private PShape engCrc;
 	private PShape medCrc;
 	private PShape frsCrc;
-	//circle to represent blink
-	private PShape blkCrc;
-	
+	// circle to represent blink
+	private PShape blinkCrc;
+
 	// Setup can be used like in the processing tool.
 	public void setup() {
 		// Set the canvas size
@@ -48,10 +47,10 @@ public class EmoApp extends PApplet {
 		medCrc = createShape(ELLIPSE, 0, 0, 20, 20);
 		frsCrc = createShape(ELLIPSE, 0, 0, 20, 20);
 		
-		blkCrc = createShape(ELLIPSE, 0, 0, 30, 30);
-		
+		blinkCrc = createShape(ELLIPSE, 0, 0, 30, 30);
+
 		// Don't draw strokes on shapes
-		noStroke();		
+		noStroke();
 		// Connect to headset
 		edkConn();
 	}
@@ -63,32 +62,32 @@ public class EmoApp extends PApplet {
 		// Redraw the background with black
 		background(0);
 
-		//set fills
-		excCrc.setFill(color(150*exc+50,0,0));
-		engCrc.setFill(color(0,150*eng+50,0));
-		medCrc.setFill(color(0,0,150*med+50));
-		frsCrc.setFill(color(150*frs+50,0,150*frs+50));
-		
-		//draw shapes
+		// set fills
+		excCrc.setFill(color(200, 0, 0));
+		engCrc.setFill(color(0, 200, 0));
+		medCrc.setFill(color(0, 0, 200));
+		frsCrc.setFill(color(200, 0, 200));
+
+		// draw shapes
 		shape(excCrc, 100, -exc * 100 + 120);
 		shape(engCrc, 200, -eng * 100 + 120);
 		shape(medCrc, 300, -med * 100 + 120);
 		shape(frsCrc, 400, -frs * 100 + 120);
 
-
-		//detect blink
+		// detect blink
 		if (blink == 1) {
-			blkX = (float) Math.random() * displayWidth;
-			blkY = (float) Math.random() * displayHeight;
-			shape(blkCrc, blkX, blkY);
-		}		
+			blinkX = (float) Math.random() * displayWidth;
+			blinkY = (float) Math.random() * displayHeight;
+			shape(blinkCrc, blinkX, blinkY);
+		}
 	}
 
 	public void edkConn() {
 		switch (option) {
 		case 1: {
-			if (Edk.INSTANCE.EE_EngineConnect("Emotiv Systems-5") != EdkErrorCode.EDK_OK.ToInt()) {
-			System.out.println("Emotiv Engine start up failed.");
+			if (Edk.INSTANCE.EE_EngineConnect("Emotiv Systems-5") != EdkErrorCode.EDK_OK
+					.ToInt()) {
+				System.out.println("Emotiv Engine start up failed.");
 				return;
 			}
 			break;
@@ -120,19 +119,21 @@ public class EmoApp extends PApplet {
 			// Log the EmoState if it has been updated
 			if (eventType == Edk.EE_Event_t.EE_EmoStateUpdated.ToInt()) {
 				Edk.INSTANCE.EE_EmoEngineEventGetEmoState(eEvent, eState);
-				//get emotion values
-				exc = EmoState.INSTANCE.ES_AffectivGetExcitementShortTermScore(eState);
-				eng = EmoState.INSTANCE.ES_AffectivGetEngagementBoredomScore(eState);
+				// get emotion values
+				exc = EmoState.INSTANCE
+						.ES_AffectivGetExcitementShortTermScore(eState);
+				eng = EmoState.INSTANCE
+						.ES_AffectivGetEngagementBoredomScore(eState);
 				med = EmoState.INSTANCE.ES_AffectivGetMeditationScore(eState);
 				frs = EmoState.INSTANCE.ES_AffectivGetFrustrationScore(eState);
-				//detect blink
+				// detect blink
 				blink = EmoState.INSTANCE.ES_ExpressivIsBlink(eState);
-				
-				//console  feedback
-				System.out.println("Excitement: "+exc);
-				System.out.println("Engagement/Boredom: "+eng);
-				System.out.println("Meditation: "+med);
-				System.out.println("Frustration: "+frs);
+
+				// console feedback
+				System.out.println("Excitement: " + exc);
+				System.out.println("Engagement/Boredom: " + eng);
+				System.out.println("Meditation: " + med);
+				System.out.println("Frustration: " + frs);
 				if (blink == 1) {
 					System.out.println("You blinked!");
 				}
